2fb86f5147a505067fb98096bf23cc28e322cf6e
KhalezovaMarianna <rct.halezova@bsu.by>
2022-08-03 10:31:08 +0300

    added components

---
+++ src/main/java/com/qaprosoft/carina/demo/gui/webPages/components/HeaderMenu.java
@@ -0,0 +1,53 @@
+package com.qaprosoft.carina.demo.gui.webPages.components;
+
+import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
+import com.qaprosoft.carina.core.gui.AbstractUIObject;
+import com.qaprosoft.carina.demo.gui.webPages.*;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+public class HeaderMenu extends AbstractUIObject {
+    @FindBy(xpath = "//*[@id=\"navbarExample\"]/ul/li[2]/a")
+    ExtendedWebElement contactBtn;
+
+    @FindBy(xpath = "//*[@id=\"navbarExample\"]/ul/li[3]/a")
+    ExtendedWebElement aboutUsBtn;
+
+    @FindBy(xpath = "//*[@id=\"cartur\"]")
+    ExtendedWebElement cartBtn;
+
+    @FindBy(xpath = "//*[@id=\"login2\"]")
+    ExtendedWebElement loginBtn;
+
+    @FindBy(xpath = "//*[@id=\"signin2\"]")
+    ExtendedWebElement signUpBtn;
+
+    public HeaderMenu(WebDriver driver, SearchContext searchContext) {
+        super(driver, searchContext);
+    }
+
+
+    public AboutUsPage goToAboutPage() {
+        aboutUsBtn.click();
+        return new AboutUsPage(getDriver());
+    }
+
+    public ContactPage goToContactPage() {
+        contactBtn.click();
+        return new ContactPage(getDriver());
+    }
+    public CartPage goToCartPage() {
+        cartBtn.click();
+        return new CartPage(getDriver());
+    }
+
+    public LogInPage goToLoginPage() {
+        loginBtn.click();
+        return new LogInPage(getDriver());
+    }
+    public SignUpPage goToSignUpPage() {
+        signUpBtn.click();
+        return new SignUpPage(getDriver());
+    }
+}
